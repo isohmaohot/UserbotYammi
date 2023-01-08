600ccd0a44f8d6a91573aa7c4ebe4792f1e5e28c
isohmaohot <120569462+isohmaohot@users.noreply.github.com>
2023-01-08 10:57:48 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 
-API_HASH = os.getenv("7457a2dd85fa14088d0bc7291143b617")
-API_ID = str(os.getenv("18986903"))
-MONGO_URI = os.getenv("mongodb+srv://cluster0.sdxhyrl.mongodb.net/Cluster0")
+API_HASH = os.getenv("")
+API_ID = str(os.getenv(""))
+MONGO_URI = os.getenv("")
 HEROKU_API = os.getenv("HEROKU_API")
 HEROKU_APP_NAME = os.getenv("HEROKU_APP_NAME")
 PY_SESSION = os.getenv("PYROGRAM_SESSION")
-TE_SESSION = os.getenv("1BVtsOJYBu2rc7jUOkQ86TZS87r7jIbcIk7ze2pNr-rP6xSWLawTpWrLdZaHEf7Ums28pyUYhDT255e28zGkrK0kslGmnokTmqXSaWIGJc5FbcEHCy5vEgE0_CaDZG8MeH83bQZpFEoOjHtAeLtehdmaW0nqydFtW4vROwRRbsIwAPPKp_5etBaImm81zeCChujpSUTcjkgNvZbMV15gapgJGZsIW1gXKWWWZgO02z-bKah4KvsP6Y8F6rWWTGpkhKqsGMbUrskaRdDhYwgmpRmnZil2gxqHQheMdDIqI8VEqKfUlicaXMNqZ505Rzm84m48zlQC61TLXas0N90PblJwJcfTKz5o=")
+TE_SESSION = os.getenv("TELETHON_SESSION")
 PREFIX = os.environ.get("PREFIX", ".")
-LOG_CHAT = str(os.getenv("-1001889953576"))
+LOG_CHAT = str(os.getenv(""))
