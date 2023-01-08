87fb5593b03636a5943d993687ba2ebdbf8f97fa
isohmaohot <sarahlaurel713@gmail.com>
2023-01-08 03:37:43 +0000

    Updates

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 
 API_HASH = os.getenv("7457a2dd85fa14088d0bc7291143b617")
-API_ID = int(os.getenv("18986903"))
-MONGO_URI = os.getenv("MONGO_URI")
+API_ID = str(os.getenv("18986903"))
+MONGO_URI = os.getenv("mongodb+srv://cluster0.sdxhyrl.mongodb.net/Cluster0")
 HEROKU_API = os.getenv("HEROKU_API")
 HEROKU_APP_NAME = os.getenv("HEROKU_APP_NAME")
 PY_SESSION = os.getenv("PYROGRAM_SESSION")
-TE_SESSION = os.getenv("TELETHON_SESSION")
+TE_SESSION = os.getenv("1BVtsOJYBu2rc7jUOkQ86TZS87r7jIbcIk7ze2pNr-rP6xSWLawTpWrLdZaHEf7Ums28pyUYhDT255e28zGkrK0kslGmnokTmqXSaWIGJc5FbcEHCy5vEgE0_CaDZG8MeH83bQZpFEoOjHtAeLtehdmaW0nqydFtW4vROwRRbsIwAPPKp_5etBaImm81zeCChujpSUTcjkgNvZbMV15gapgJGZsIW1gXKWWWZgO02z-bKah4KvsP6Y8F6rWWTGpkhKqsGMbUrskaRdDhYwgmpRmnZil2gxqHQheMdDIqI8VEqKfUlicaXMNqZ505Rzm84m48zlQC61TLXas0N90PblJwJcfTKz5o=")
 PREFIX = os.environ.get("PREFIX", ".")
-LOG_CHAT = int(os.getenv("LOG_CHAT"))
+LOG_CHAT = str(os.getenv("-1001889953576"))
